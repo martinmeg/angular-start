16ce6895cd8fbb7577d9e5828f1446e8715a24dd
Martin Guignard <martinmeg.ar@gmail.com>
2015-12-12 10:37:28 -0300

    First use of controller with  variables

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the startApp
  */
 angular.module('startApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+  	var movie = {
+  		title: 'Skyfall',
+  		image: 'http://vignette4.wikia.nocookie.net/jamesbond/images/b/b3/Skyfall_Logo.jpg',
+  		description: 'James Bond movie'
+  	}
+    $scope.movieTitle = movie.title;
+    $scope.movieDescription = movie.description;
   });
