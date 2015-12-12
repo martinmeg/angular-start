d3355a79b7b7c08bee0b6b50c702ec8aa2b8df42
Martin Guignard <martinmeg.ar@gmail.com>
2015-12-12 10:51:09 -0300

    Applied ng-repeat

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,18 @@
  */
 angular.module('startApp')
   .controller('MainCtrl', function ($scope) {
-  	var movie = {
+  	var movieList = [{
   		title: 'Skyfall',
   		image: 'http://vignette4.wikia.nocookie.net/jamesbond/images/b/b3/Skyfall_Logo.jpg',
-  		description: 'James Bond movie'
-  	}
-    $scope.movie = movie;
+  		description: 'James Bond movie 1'
+  	}, {
+  		title: 'Spectre',
+  		image: 'http://vignette1.wikia.nocookie.net/jamesbond/images/0/0e/Spectre_Logo.png',
+  		description: 'James Bond movie 2'
+  	}, {
+  		title: 'Casino Royale',
+  		image: 'http://vignette2.wikia.nocookie.net/logopedia/images/9/92/Casino_Royale_Logo.jpg',
+  		description: 'James Bond movie 3'
+  	}];
+    $scope.movies = movieList;
   });
